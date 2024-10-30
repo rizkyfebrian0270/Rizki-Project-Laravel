d5c2ae0bf5d7032a2c41a52cc145358e3778487f
Rizki Febrian <541231191@student.smktelkom-pwt.sch.id>
2024-10-30 11:12:56 +0700

    laravel initial commit

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('index');
-});
+// Route::get('/', function () {
+//    return view('index');
+// });
 
 Route::get('/welcome', function () {
-    return view('welcome');
+   return view('welcome');
 });
 
-Route::get('/about', function () {
-    return "Tentang Gue";
-});
+// Route::get('/about', function () {
+//    return "Tentang Gue";
+// });
+
+Route::get('/', [HomeController::class, "index"])->name('home');
+Route::get('/about', [HomeController::class, "about"])->name('about');
